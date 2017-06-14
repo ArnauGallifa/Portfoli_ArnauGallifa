4487fb4e3258131fc0083f4920b3f6e2e9942c65
ArnauGallifa <arnaugallifa@gmail.com>
2017-06-14 20:33:36 +0200

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,35 @@
+function go_3D(){
+    window.location.href="pag_3D.html";   
+}
+
+function go_animation(){
+    window.location.href="pag_animation.html";   
+}
+
+function go_ilustration(){
+    window.location.href="pag_ilustration.html";   
+}
+
+function go_rigging(){
+    window.location.href="pag_rigging.html";   
+}
+
+function go_lighting(){
+    window.location.href="pag_lighting.html";   
+}
+
+function go_CSSandHTML(){
+    window.location.href="pag_CSSandHTML.html";   
+}
+
+function go_about(){
+    window.location.href="pag_about.html";
+}
+
+function go_contact(){
+    window.location.href="pag_contact.html";
+}
+
+function go_index(){
+    window.location.href="index.html";
+}
